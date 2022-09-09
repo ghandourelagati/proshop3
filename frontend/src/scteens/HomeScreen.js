1217e8328f578fc51cb8cf33242ab048c7e3701b
Ahmed Ghandour <ahmedghandour8@gmail.com>
2022-09-09 08:50:28 +0200

    Fist commit

---
+++ frontend/src/scteens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import products from '../products'
+
+const HomeScreen = () => {
+  return (
+      <div>
+          <h1>Latest Products</h1>
+          
+      </div>
+  )
+}
+
+export default HomeScreen

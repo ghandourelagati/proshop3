cef22717ef437af72cf85129566c26ce955d0f20
Ahmed Ghandour <ahmedghandour8@gmail.com>
2022-09-10 02:24:59 +0200

    Rating, product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome to Proshop</h1> 
-        
+    <div >
+      <Header />
+      <main className='py-3'>
+      <Container>
+<HomeScreen />
+        </Container>
+        </main> 
+     <Footer />  
     </div>
   );
 }
